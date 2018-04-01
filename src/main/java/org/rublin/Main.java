f1776827e17ac98af1e5dea7e8ac4e3e6aa4ef30
rublin <toor.ua@gmail.com>
2018-04-01 14:55:59 +0300

    works except jsp

--- src/main/java/org/rublin/Main.java
+++ src/main/java/org/rublin/Main.java
@@ -2,11 +2,8 @@ package org.rublin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-//@ComponentScan
 @SpringBootApplication
 @EnableScheduling
 public class Main {
---
+++ src/main/java/org/rublin/configuration/SecurityBeanConfig.java
@@ -0,0 +1,64 @@
+package org.rublin.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.rublin.model.user.User;
+import org.rublin.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityBeanConfig extends GlobalAuthenticationConfigurerAdapter {
+
+    private UserRepository userRepository;
+
+    @Override
+    public void init(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService());
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+                User user = userRepository.getByEmail(s);
+                if (user != null) {
+                    return new org.springframework.security.core.userdetails.User(user.getEmail(),
+                            user.getPassword(),
+                            true,
+                            true,
+                            true,
+                            true,
+                            AuthorityUtils.createAuthorityList(user.getRoles().iterator().next().name()));
+                } else {
+                    throw new UsernameNotFoundException("Could not find the user " + s);
+                }
+            }
+        };
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        System.out.println(encoder.encode("P@ssw0rd"));
+        return encoder;
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+}
