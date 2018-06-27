f7d2cda1b7a2e1bcb0d8fb4942ed22cabd41b2a1
Ruslan Sheremet <toor.ua@gmail.com>
2018-06-27 16:07:22 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 #! /bin/bash
+# validate parameters
+if [ -z "$2" ]
+        then
+                echo "usage: install.sh <telegram bot name> <telegram bot token>"
+        exit 1
+fi
+# create variables
+# postgresPass=$1
+telegramBot=$1
+telegramToken=$2
+
 # update server
-apt-get --assume-yes update
+#apt-get --assume-yes update
 # install dependencies
-apt-get --assume-yes install openjdk-8-jre postgresql-9.5 vlc-nox
+#apt-get --assume-yes install openjdk-8-jre postgresql-9.5 vlc-nox
+mkdir -p /opt/smartspace/config
