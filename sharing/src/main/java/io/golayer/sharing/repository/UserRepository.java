0aec9d8da8af0c4d37aac31c6273c09351f97539
rublin <toor.ua@gmail.com>
2019-06-08 18:13:32 +0300

    remove wrong commit

--- sharing/src/main/java/io/golayer/sharing/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package io.golayer.sharing.repository;
-
-import io.golayer.sharing.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-}
